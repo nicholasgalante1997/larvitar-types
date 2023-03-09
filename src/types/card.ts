59e4d933e0459ad2df51ecbcea28cfa6fe28afd6
Nick G <nicholasgalante1997@gmail.com>
2023-03-08 23:17:33 -0700

    updated types

--- src/types/card.ts
+++ src/types/card.ts
@@ -2,17 +2,17 @@ export type Card = {
     id: string;
     name: string;
     supertype: string;
-    subtypes: string[];
-    level: string;
+    subtypes?: string[];
+    level?: string;
     hp: string;
     types: string[];
-    evolvesFrom: string;
-    evolvesTo: string[];
-    rules: string[];
-    abilities: Ability[];
+    evolvesFrom?: string;
+    evolvesTo?: string[];
+    rules?: string[];
+    abilities?: Ability[];
     attacks: Attack[];
-    weaknesses: Weakness[];
-    resistances: Resistance[];
+    weaknesses?: Weakness[];
+    resistances?: Resistance[];
     retreatCost: string[];
     set: TCGSet;
     number: string;
@@ -64,10 +64,25 @@ type Resistance = {
     value: string;
 };
 
-type TCGSet = {};
+type TCGSet = {
+    id: string;
+    name: string;
+    series: string;
+    printedTotal: number;
+    total: number;
+    ptcgoCode: string;
+    releaseDate: string;
+    updatedAt: string;
+    images: {
+        symbol: string;
+        logo: string;
+    }
+};
 
 export const CARD_ENDPOINT = 'https://api.pokemontcg.io/v2/cards/%id%' as const;
 export const CARDS_ENDPOINT = 'https://api.pokemontcg.io/v2/cards' as const;
+
+export type SwordAndShieldSet = `swsh${number}-${number}` | `swsh${number}tg-TG${number}`;
 
 /** Query string to match cards against: See https://docs.pokemontcg.io/api-reference/cards/search-cards#query-parameters */
 export type CARDS_ENDPOINT_OPTIONS = {
